3b5659a9743e30b31839a754a2c62b484b92b0c2
S-Makrod <53048085+S-Makrod@users.noreply.github.com>
2021-07-07 14:33:58 -0400

    Add files via upload

---
+++ javascript/main.js
@@ -0,0 +1,31 @@
+function dropDown() {
+  document.getElementById("myDropdown").classList.toggle("show");
+}
+  
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
+
+function navHeight(){
+  if(window.innerWidth >= 800){
+    var body = document.body, html = document.documentElement;
+    var height = Math.max( body.offsetHeight, html.clientHeight, html.offsetHeight );
+
+    document.getElementById("nav").style.height = height + "px";
+  } else {
+    document.getElementById("nav").style.height = "10vh";
+  }
+}
+
+window.onload = navHeight;
+
+window.onresize = function(){ location.reload(); }
