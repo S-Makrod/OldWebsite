8108b7a8225d8451a2aec79640268228371de0fc
S-Makrod <53048085+S-Makrod@users.noreply.github.com>
2021-07-07 11:44:24 -0400

    Add files via upload

---
+++ javascript/main.js
@@ -0,0 +1,25 @@
+function dropDown() {
+  document.getElementById("myDropdown").classList.toggle("show");
+}
+  
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
+
+window.onload = function() {
+  if(window.innerWidth >= 800){
+    var body = document.body, html = document.documentElement;
+    var height = Math.max( body.offsetHeight, html.clientHeight, html.offsetHeight );
+
+    document.getElementById("nav").style.height = height + "px";
+  }
+}
