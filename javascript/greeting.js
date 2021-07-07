77a237e476f458b5f70ff78282dbcc11eb2004e2
S-Makrod <53048085+S-Makrod@users.noreply.github.com>
2021-07-07 08:36:56 -0400

    Add files via upload

---
+++ javascript/greeting.js
@@ -0,0 +1,14 @@
+window.onload = function (){
+    var d = new Date();
+    var y = "I am Saad, a student at the University of Toronto studying Computer Science. Enjoy your exploration through my website!"
+
+    if(d.getHours() < 12){
+        document.getElementById('greeting').innerHTML = "Good Morning! " + y;
+    } else if(12 <= d.getHours() && d.getHours() <= 17){
+        document.getElementById('greeting').innerHTML = "Good Afternoon! " + y;
+    } else if(17 < d.getHours() && d.getHours() <= 20){
+        document.getElementById('greeting').innerHTML = "Good Evening! " + y;
+    } else if(20 < d.getHours()){
+        document.getElementById('greeting').innerHTML = "Wow it's pretty late! " + y;
+    }
+}
